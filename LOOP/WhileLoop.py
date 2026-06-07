434d8692a4d925ac75c36af2baef5841146a53b1
Yurish2 <gurungyurish@gmail.com>
2026-06-07 23:05:51 +0545

    changed the file location

---
+++ LOOP/WhileLoop.py
@@ -0,0 +1,15 @@
+i=1
+while i<= 5:
+    print (i)
+    i =i+1
+
+
+n=int(input("enter a number :"))
+i=1
+
+while i<=n :
+    
+    print (i)
+    i=i+1
+
+print("doneee")
---
+++ TASKS/ConditionalsTask/Task4.py
@@ -0,0 +1,17 @@
+name=input("enter your name :").lower()
+
+if name=="yurish":
+
+    date=int(input("Please enter your date of birth :"))
+    
+    Cdate=int(input("enter the current year :"))
+
+    age=(Cdate-date)+10
+
+    print("your after 10 year is"+ str(age)) 
+
+
+
+else:
+    print("invalid name")
+    
