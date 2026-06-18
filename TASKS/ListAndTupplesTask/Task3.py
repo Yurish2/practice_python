3cf6a89225caf6ce33040f7dbb833ac667e4c4dc
Yurish2 <gurungyurish@gmail.com>
2026-06-18 15:43:41 +0545

    list and tupple

---
+++ TASKS/ListAndTupplesTask/Task3.py
@@ -0,0 +1,13 @@
+# program to count the number a element in list 
+
+alpha=[ "A", "B", "C", "D", "E", "E", "A", "E", "D"]
+
+print(alpha.count("E"))
+
+#program to print assending order
+
+asse =["a","b" , "c", "a", "b", "e", "d"]
+
+asse.sort()
+
+print(asse)
